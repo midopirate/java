beb7e16597ec42e91b49668a228ff0fddc01841c
midopirate <dj.dhriwa2013@gmail.com>
2018-10-02 21:35:14 -0700

    Update and rename form.js to foxyform.js

---
+++ foxyform.js
@@ -0,0 +1,12 @@
+(function(d){
+	  var l = d.getElementById("foxyform_embed_link_935852");
+	      i = d.createElement('IFRAME');
+	  i.src = "https://www.foxyform.com/form.php?id=935852&sec_hash=abd79707ef8";
+	  i.width = "350px";
+	  i.height = "521px";
+	  i.frameborder = 0;
+	  i.setAttribute('style', 'border: none;');
+	  i.setAttribute('frameborder', '0');
+	  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
+	  l.parentNode.insertBefore(i,l);
+	})(document);
